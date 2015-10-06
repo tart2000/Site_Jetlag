fdbbb0571f3ecf457c72a6359fbe81f12c7a6d27
Chesnel <pa@chesnel.ca>
2015-10-06 10:11:04 -0400

    add animation for number

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,16 @@
+$(document).ready(function(){
+	var location = window.location.pathname; //get Current link
+	// var $currentNumber = $('.number.active');
+	$('.number').hover(function(){
+		// console.log('a');
+		$('.number').removeClass('active');
+	});
 
+	$('.number').mouseleave(function(){
+		// console.log('a');
+		console.log('[data-link="'+location+'"]');
+		$('[data-link="'+location+'"]').addClass('active');
+	});
+
+
+});
